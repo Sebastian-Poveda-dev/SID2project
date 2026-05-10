e1f9f54db4ba7b80e4cf403b3c25a0bd58243850
Sebastian-Poveda-dev <1006109183@u.icesi.edu.co>
2026-05-09 20:15:53 -0500

    refacotring directories hierarchy

--- backend/src/main/java/com/uniplan/security/JwtAuthenticationEntryPoint.java
+++ backend/src/main/java/com/uniplan/security/JwtAuthenticationEntryPoint.java
@@ -1,6 +1,6 @@
 package com.uniplan.security;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import tools.jackson.databind.ObjectMapper;
 import com.uniplan.dto.response.ErrorResponseDTO;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
